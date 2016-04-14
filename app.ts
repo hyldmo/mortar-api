324a72fb0914f4560e314110e2364cc3a4cd5c48
eivindh <eivind_h@outlook.com>
2016-04-14 21:43:33 +0200

    Added landing page

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 ﻿import express = require('express');
-import routes = require('./routes/index');
-import user = require('./routes/user');
+
 import http = require('http');
 import path = require('path');
 
@@ -9,7 +8,7 @@ export var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.favicon());
+app.use(express.favicon('images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -22,9 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
+import routes = require('./routes/recipes');
 
-app.get('/', routes.index);
-app.get('/recipe', routes.recipe);
+app.use('/', routes);
+
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- routes/recipes.ts
+++ routes/recipes.ts
@@ -5,29 +5,30 @@ var url = 'mongodb://localhost:27017/topkook';
 // Use connect method to connect to the Server
 var findDocuments = function (db, callback) {
     // Get the documents collection
-    var collection = db.collection('recipe');
+    var collection = db.collection('recipes');
     // Find some documents
     collection.find({}).toArray(function (err, docs) {
-        console.log("Found the following records");
+        console.log('Found the following records');
         console.dir(docs);
         callback(docs);
     });
 }
 
-import express = require('express');
+var express = require('express');
 
-export function index(req: express.Request, res: express.Response) {
-    res.json({ title: 'Express' });
-};
+var router = express.Router();
 
-export function recipe(req: express.Request, resp: express.Response) {
+router.get('/', function (req, resp) {
+
+    console.log('Connected correctly to server');
     MongoClient.connect(url, function (err, db) {
-        console.log("Connected correctly to server");
+        console.log('Connected correctly to server');
         
         findDocuments(db, function (docs) {
             resp.json(docs);
             db.close();
         });
     });
-    
-};
+});
+
+export = router;
