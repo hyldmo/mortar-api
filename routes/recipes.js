f3fcfd71047e707a0ae81e6755de3f741a451af6
eivindh <eivind_h@outlook.com>
2016-04-15 12:32:12 +0200

    Removed javascript files from gitignore as typescript is not being used

---
+++ routes/recipes.js
@@ -0,0 +1,56 @@
+﻿var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017/topkook';
+// Collection Name
+var collName = 'recipes';
+
+
+function findDocuments (callback) {
+    MongoClient.connect(url, function(err, db) {
+        console.log('Connected correctly to server');
+        // Get the documents collection
+        var collection = db.collection(collName);
+        // Find some documents
+        collection.find({}).toArray(function(err, docs) {
+            callback(docs);
+            db.close();
+        });
+    });
+}
+
+function findOne (id, callback) {
+    MongoClient.connect(url, function(err, db) {
+        console.log('Connected correctly to server');
+        // Get the documents collection
+        var collection = db.collection(collName);
+
+        // Find some documents
+        collection.find({}).toArray(function (err, docs) {
+            callback(docs[id]);
+            db.close();
+        });
+    });
+}
+
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function (req, resp) {
+    
+    findDocuments(function (docs) {
+        resp.json(docs);
+    });
+});
+router.get('/:id', function (req, resp, next) {
+    findOne(req.params.id, function (docs) {
+        if (docs !== undefined)
+            resp.json(docs);
+        else
+            return next();
+    });
+});
+
+
+module.exports = router;
