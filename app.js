d0026105c3cef801488a2df2fbc90832dd8d5e85
eivindh <eivind_h@outlook.com>
2016-04-15 17:10:28 +0200

    Changed url to plural

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/recipe', recipes);
+app.use('/recipes', recipes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
