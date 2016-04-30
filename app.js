b498fa983957ec83940fa2365f6a9fe04e555169
eivindh <eivind_h@outlook.com>
2016-04-30 22:28:27 +0200

    Fixed project

--- app.js
+++ app.js
@@ -13,11 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(favicon('public/images/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
