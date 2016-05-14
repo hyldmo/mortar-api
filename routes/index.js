2b50d13964bd1ca53f40d486ebe3ef00654e7331
eivindh <eivind_h@outlook.com>
2016-05-14 21:05:55 +0200

    Added cat facts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Api documentation' });
 });
+
+router.get('/catfact', function(req, res, next) {
+    http.get('http://catfacts-api.appspot.com/api/facts', function(response){
+        var body = '';
+
+        response.on('data', function(chunk){
+            body += chunk;
+        });
+
+        response.on('end', function() {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.json(JSON.parse(body));
+        });
+    }).on('error', function(e){
+        next(e);
+    });
+});
+
 
 module.exports = router;
