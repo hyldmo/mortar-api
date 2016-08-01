0fd7497cd65c80ecc4808817bba2a8575a486486
eivhyl <eivind_h@outlook.com>
2016-08-01 19:44:20 +0200

    Added api passthrough to recipepuppy

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,32 @@ router.get('/catfact', function(req, res, next) {
     });
 });
 
+router.get('/puppy', function(req, res, next) {
+    //Get query parameters
+    var i = req.url.indexOf('?');
+    var queryStr = i != -1 ? req.url.substr(i):  '';
+
+    http.get('http://www.recipepuppy.com/api' + queryStr, function(response){
+        var body = '';
+
+        response.on('data', function(chunk){
+            body += chunk;
+        });
+
+        response.on('end', function() {
+            res.header('Access-Control-Allow-Origin', '*');
+
+            try {
+                res.json(JSON.parse(body));
+            } catch (e) {
+                next(e);
+            }
+        });
+    }).on('error', function(e){
+        next(e);
+    });
+});
+
+
 
 module.exports = router;
